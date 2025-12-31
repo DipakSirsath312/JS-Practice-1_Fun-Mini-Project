89512ca9b8ed735892ec54aedb5cc6aa3d44ad20
DipakSirsath312 <dipaksirsath312@gmail.com>
2025-12-31 19:56:50 +0530

    [ADDED]:Dipak - Complete The JS Practice 1 also Master JavaScript

---
+++ JS Practice1_Mini Project/script.js
@@ -0,0 +1,43 @@
+// 1. Make a Rectangle and when we move mouse on the rectangle on the left side we want to gradually change the color
+// of the rectangle to red and when we move mouse to the right side we want to change the color to blue.
+
+var rect = document.querySelector("#center");
+
+rect.addEventListener("mousemove", function (details) {
+
+    var rectangleLocation = rect.getBoundingClientRect();
+    var insideRectVal = details.clientX - rectangleLocation.left;
+
+    if (insideRectVal < rectangleLocation.width / 2) {
+        var redColor = gsap.utils.mapRange(
+            0, 
+            rectangleLocation.width / 2, 
+            255, 
+            0, 
+            insideRectVal
+        );
+        gsap.to(rect, {
+            backgroundColor: `rgb(${redColor}, 0, 0)`,
+            ease: Power4,
+        });
+    } else {
+        var blueColor = gsap.utils.mapRange(
+            rectangleLocation.width / 2, 
+            rectangleLocation.width, 
+            0, 
+            255, 
+            insideRectVal
+        );
+        gsap.to(rect, {
+            backgroundColor: `rgb(0, 0, ${blueColor})`,
+            ease: Power4,
+        });
+    }
+})
+
+
+rect.addEventListener("mouseleave", function () {
+    gsap.to(rect, {
+        backgroundColor: "White",
+    })
+}) 
